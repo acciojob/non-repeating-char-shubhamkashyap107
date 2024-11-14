c198430c0979698f9ab33d6cd392c633fb634bd8
Shubham Kashyap <91467406+shubhamkashyap107@users.noreply.github.com>
2024-11-14 17:13:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
-if(!str)
-{
-	return null
-}
-	for(let i = 0; i < str.length - 1; i++)
-	{
-		let flag = false;
-		for(let j = i + 1; j < str.length; j++)
-			{
-				if(str.charAt(i) == str.charAt(j))
-				{
-					flag = true;	
-				}
-			}
 
-		if(flag == false)
-		{
-			return str.charAt(i)
-		}
-	}
+	if(!str)
+        {
+            return null
+        }
 
-	return null
+       let count = {}
+
+
+       for(let i = 0; i < str.length; i++)
+       {
+        let char = str.charAt(i)
+        // console.log(char)
+        if(!count[char])
+        {
+            count[char] = 1
+        }
+        else
+        {
+            count[char] = count[char] + 1
+        }
+       }
+
+       for(let item in count)
+       {
+        if(count[item] == 1)
+        {
+            return item
+        }
+       }
+
+
+       return null
 	
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
