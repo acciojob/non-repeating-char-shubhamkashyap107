d7fd82d333353c985daf32869adb6a06604bbd68
Shubham Kashyap <91467406+shubhamkashyap107@users.noreply.github.com>
2024-11-14 17:13:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,5 +34,5 @@ function firstNonRepeatedChar(str) {
        return null
 	
 }
-// const input = prompt("Enter a string");
-// alert(firstNonRepeatedChar(input)); 
+const input = prompt("Enter a string");
+alert(firstNonRepeatedChar(input)); 
