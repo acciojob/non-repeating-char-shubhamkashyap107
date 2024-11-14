df5f6618f209d525b8d68fe763c180f37dc79cd6
Shubham Kashyap <91467406+shubhamkashyap107@users.noreply.github.com>
2024-11-14 16:49:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+
+	for(let i = 0; i < str.length - 1; i++)
+	{
+		let flag = false;
+		for(let j = i + 1; j < str.length; j++
+			{
+				if(str.chartAt(i) == str.charAt(j))
+				{
+					flag = true;	
+				}
+			}
+
+		if(flag == false)
+		{
+			return str.charAt(i)
+		}
+	}
+
+	return null
+	
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
